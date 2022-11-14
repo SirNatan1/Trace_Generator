6c0449ec29412658b9dd96d12c7b17bf1e871ee9
SirNatan1 <106090674+SirNatan1@users.noreply.github.com>
2022-11-14 09:51:02 +0200

    Added: Docker, app, and the tracing
    Added: Docker, app, and the tracing

---
+++ tracing.js
@@ -0,0 +1,22 @@
+const opentelemetry = require("@opentelemetry/sdk-node");
+const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
+const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
+
+// For troubleshooting, set the log level to DiagLogLevel.DEBUG
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+
+const {
+  OTLPTraceExporter,
+} = require("@opentelemetry/exporter-trace-otlp-http");
+
+const sdk = new opentelemetry.NodeSDK({
+  traceExporter: new OTLPTraceExporter({
+    // optional - url default value is http://localhost:4318/v1/traces
+    url: "http://172.17.0.1:4318/v1/traces",
+    // optional - collection of custom headers to be sent with each request, empty by default
+    headers: {},
+  }),
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+sdk.start()
